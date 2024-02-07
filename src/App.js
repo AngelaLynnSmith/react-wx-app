59885d06942b9aec858bad81223aebdfc0a10602
Angela Lynn Smith <angiesmith1226@gmail.com>
2024-02-06 20:27:25 -0500

    added GitHub and Netlify links

--- src/App.js
+++ src/App.js
@@ -3,7 +3,34 @@ import React from "react";
 import './App.css';
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+		<div className="App">
+			<h1>React Weather App</h1>
+			<footer>
+				This project was coded by{" "}
+				<a
+					rel="noreferrer"
+					href="https://github.com/AngelaLynnSmith"
+					target="blank">
+					Angela Lynn Smith,
+				</a>{" "}
+				is open-sourced on{" "}
+				<a
+					rel="noreferrer"
+					href="https://github.com/AngelaLynnSmith/react-wx-app"
+					target="blank">
+					Github,
+				</a>{" "}
+				and is hosted on{" "}
+				<a
+					rel="noreferrer"
+					href="https://react-wx-app-2024.netlify.app/"
+					target="blank">
+					Netlify.
+				</a>
+			</footer>
+		</div>
+	);
 }
 
 
