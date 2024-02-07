3209f553a9b8035ed65fa1d05924b8a257281c4a
Angela Lynn Smith <angiesmith1226@gmail.com>
2024-02-06 20:54:36 -0500

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -5,30 +5,32 @@ import './App.css';
 export default function App() {
   return (
 		<div className="App">
-			<h1>React Weather App</h1>
-			<footer>
-				This project was coded by{" "}
-				<a
-					rel="noreferrer"
-					href="https://github.com/AngelaLynnSmith"
-					target="blank">
-					Angela Lynn Smith,
-				</a>{" "}
-				is open-sourced on{" "}
-				<a
-					rel="noreferrer"
-					href="https://github.com/AngelaLynnSmith/react-wx-app"
-					target="blank">
-					Github,
-				</a>{" "}
-				and is hosted on{" "}
-				<a
-					rel="noreferrer"
-					href="https://react-wx-app-2024.netlify.app/"
-					target="blank">
-					Netlify.
-				</a>
-			</footer>
+			<div className="container">
+				<h1>React Weather App</h1>
+				<footer>
+					This project was coded by{" "}
+					<a
+						rel="noreferrer"
+						href="https://github.com/AngelaLynnSmith"
+						target="blank">
+						Angela Lynn Smith,
+					</a>{" "}
+					is open-sourced on{" "}
+					<a
+						rel="noreferrer"
+						href="https://github.com/AngelaLynnSmith/react-wx-app"
+						target="blank">
+						Github,
+					</a>{" "}
+					and is hosted on{" "}
+					<a
+						rel="noreferrer"
+						href="https://react-wx-app-2024.netlify.app/"
+						target="blank">
+						Netlify.
+					</a>
+				</footer>
+			</div>
 		</div>
 	);
 }
