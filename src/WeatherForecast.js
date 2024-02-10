c4bab3c8d103f35983b9de55f0def63d33e5585a
Angela Lynn Smith <angiesmith1226@gmail.com>
2024-02-09 22:03:07 -0500

    built 5 day forecast

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -4,36 +4,40 @@ import axios from "axios";
 import WeatherForecastDay from "./WeatherForecastDay";
 
 export default function WeatherForecast(props) {
-    let [loaded, setLoaded] = useState(false);
-    let [forecast, setForecast] = useState(null);
+	let [loaded, setLoaded] = useState(false);
+	let [forecast, setForecast] = useState(null);
 
-    function handleResponse(response) {
-        setForecast(response.data.daily);
-        setLoaded(true);
-    }
-    
-    if (loaded) {
-         return (
-		<div className="WeatherForecast">
-		<div className="row">
-		<div className="col">
-     <WeatherForecastDay data={forecast[0]} />
-		</div>
-	</div>
-</div>
-);
-      
+	function handleResponse(response) {
+		setForecast(response.data.daily);
+		setLoaded(true);
+	}
 
-    
-    } else {
-          let apiKey = "a5acb752426cd8188485c35694980e3a";
-					let longitude = props.coordinates.lon;
-					let latitude = props.coordinates.lat;
-					let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=imperial`;
+	if (loaded) {
+		return (
+			<div className="WeatherForecast">
+				<div className="row">
+					{forecast.map(function (dailyForecast, index) {
+						if (index < 5) {
+							return (
+								<div
+									className="col"
+									key={index}>
+									<WeatherForecastDay data={dailyForecast} />
+								</div>
+							);
+						}
+					})}
+				</div>
+			</div>
+		);
+	} else {
+		let apiKey = "e6c2364656962bdcb16bc352fc42569a";
+		let longitude = props.coordinates.lon;
+		let latitude = props.coordinates.lat;
+		let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=imperial`;
 
-        axios.get(apiUrl).then(handleResponse);
-        
-            return null;
-       
-    }
+		axios.get(apiUrl).then(handleResponse);
+
+		return null;
+	}
 }
