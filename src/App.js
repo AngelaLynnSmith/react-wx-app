865b7c3886c41ad664f67b4fbdfb2a40f2b485b1
Angela Lynn Smith <angiesmith1226@gmail.com>
2024-02-10 09:32:59 -0500

    Revert "changed default city"
    This reverts commit 1019a543e118945ec1cde735663cdb17e786660f.

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ export default function App() {
 	return (
 	<div className="App">
 			<div className="container">
-				<Weather defaultCity="Warner Robins" />
+				<Weather defaultCity="Atlanta" />
 				<footer className="footer">
 					This project was coded by{" "}
 					<a
