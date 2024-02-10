e4e96970d762100f0d2e4bf706b6669379755fe6
Angela Lynn Smith <angiesmith1226@gmail.com>
2024-02-10 08:03:03 -0500

    adjusted search box size

--- src/Weather.js
+++ src/Weather.js
@@ -43,7 +43,7 @@ export default function Weather(props) {
 					<div className="Weather">
 						<form onSubmit={handleSubmit}>
 							<div className="row">
-								<div className="col-9">
+								<div className="col-6">
 									<input
 										type="search"
 										placeholder="Enter a city..."
