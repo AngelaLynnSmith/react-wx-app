dee170663f0ade3eeae72809a7919ae037db1c93
Angela Lynn Smith <angiesmith1226@gmail.com>
2024-02-09 22:36:39 -0500

    refreshed the forecast

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,17 +1,23 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./WeatherForecast.css";
 import axios from "axios";
 import WeatherForecastDay from "./WeatherForecastDay";
+import { cleanup } from "@testing-library/react";
 
 export default function WeatherForecast(props) {
 	let [loaded, setLoaded] = useState(false);
-	let [forecast, setForecast] = useState(null);
+    let [forecast, setForecast] = useState(null);
+    
+
+    useEffect(() => {
+        setLoaded(false);
+     }, [props.coordinates]);
 
 	function handleResponse(response) {
 		setForecast(response.data.daily);
 		setLoaded(true);
-	}
-
+    }
+    
 	if (loaded) {
 		return (
 			<div className="WeatherForecast">
--- src/WeatherTemperature.js
+++ src/WeatherTemperature.js
@@ -1,45 +1,13 @@
 import React, {useState}  from "react";
 
 export default function WeatherTemperature(props) {
-    const [unit, setUnit] = useState("fahrenheit");
-    function showCelsius(event) {
-        event.preventDefault();
-        setUnit("celsius")
-    }
-
-    function showFahrenheit(event) {
-        event.preventDefault();
-        setUnit("Fahrenheit");
-
-    }
-
-    function celsius() {
-    return ((props.fahrenheit - 32) * 5/9);
-    }
-
-	if (unit === "fahrenheit") {
-		return (
+return (
 			<div className="WeatherTemperature">
-				<span className="temperature">{Math.round(props.fahrenheit)}</span>
+		<span className="temperature">{Math.round(props.fahrenheit)}</
+		span>
 				<span className="unit">
-					°F |{" "}
-					<a href="/" onClick={showCelsius}>
-					°C
-					</a>
-				</span>
-			</div>
-		);
-    } else {
-		return (
-			<div className="WeatherTemperature">
-				<span className="temperature">{Math.round(celsius())}</span>
-				<span className="unit">
-					<a href="/" onClick={showFahrenheit}>
 					°F
-					</a>{" "}
-					| °C
 				</span>
 			</div>
 		);
 	}
-}
