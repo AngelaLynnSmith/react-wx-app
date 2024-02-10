139ccd1d71d021977d6ac95b2900f2af883725ab
Angela Lynn Smith <angiesmith1226@gmail.com>
2024-02-10 09:33:12 -0500

    Revert "made more responsive"
    This reverts commit ab971d22e35032d158619a068f9cccd86eef3399.

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ export default function App() {
 	<div className="App">
 			<div className="container">
 				<Weather defaultCity="Atlanta" />
-				<footer className="footer">
+				<footer>
 					This project was coded by{" "}
 					<a
 						href="https://github.com/AngelaLynnSmith"
