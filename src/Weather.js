e208512aa6697041b673f578454d45c87b7b124b
Angela Lynn Smith <angiesmith1226@gmail.com>
2024-02-11 09:32:02 -0500

    updated search placeholder name

--- src/Weather.js
+++ src/Weather.js
@@ -46,7 +46,7 @@ export default function Weather(props) {
 								<div className="col-9">
 									<input
 										type="search"
-										placeholder="Enter a city..."
+										placeholder="Enter a city name..."
 										className="form-control"
 								autoFocus="on"
 								onChange={handleCityChange}
